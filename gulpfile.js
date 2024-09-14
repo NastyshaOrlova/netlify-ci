40353488f00748022e7e3f0168f2b945124fc655
Nastya Orlova <web.orlova.dev@gmail.com>
2024-09-14 11:09:39 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat-css');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const { parallel } = require('gulp');
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+  });
+}
+
+function html() {
+  return gulp
+    .src('src/**/*.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function css() {
+  return gulp
+    .src('src/blocks/**/*.css')
+    .pipe(plumber())
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function images() {
+  return gulp
+    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function fonts() {
+  return gulp
+    .src('src/fonts/**/*.{woff,woff2,eot,ttf,otf}')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function clean() {
+  return del('dist');
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts));
+
+function watchFiles() {
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/blocks/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/fonts/**/*.{woff,woff2,eot,ttf,otf}}'], fonts);
+}
+const watchapp = parallel(build, watchFiles, serve);
+
+exports.html = html;
+exports.css = css;
+exports.images = images;
+exports.fonts = fonts;
+exports.clean = clean;
+exports.build = build;
+exports.watchapp = watchapp;
+
+exports.default = watchapp;
